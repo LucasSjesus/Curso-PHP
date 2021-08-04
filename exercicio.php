22c7badeb29c1436a1dbb8dab3fe7b4609fc1b24
LucasSjesus <lucassjesus@hotmail.com>
2021-08-04 17:21:02 -0300

    Transformando em PHP

---
+++ exercicio.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+    
+    
+    <title>Exercicio</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> 
+            class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            <?php
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS © <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
+        
+
+
+
+
+        
+    <footer class="rodape">
+        COD3R & ALUNOS &copy; <?= date('Y');?>
+    </footer>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+    
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 01 - Básico</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+                    </ul>
+                
+                </div>
+            </nav>
+        </div>
+    </main>
+        
+
+
+
+
+        
+    <footer class="rodape">
+        COD3R & ALUNOS &copy; <?= date('Y');?>
+    </footer>
+    
+</body>
+</html>
